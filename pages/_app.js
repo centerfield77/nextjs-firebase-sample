6d590b7bf3e789d10f11447a5fa246080ff53523
nakano_mac <tatsunori.dia@gmail.com>
2021-06-12 14:39:50 +0900

    🔥

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,3 @@
-import UserProvider from '../context/userContext'
-
-// Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  )
+  return <Component {...pageProps} />;
 }
